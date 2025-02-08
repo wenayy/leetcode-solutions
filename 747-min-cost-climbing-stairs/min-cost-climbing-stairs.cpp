817655e5e9ab205e60df124998da64f2fb17c542
vinay <vinayjjoshi.310@gmail.com>
2025-02-09 03:10:59 +0530

    Time: 4 ms (6.75%) | Memory: 31.9 MB (5.07%) - LeetSync

--- 747-min-cost-climbing-stairs/min-cost-climbing-stairs.cpp
+++ 747-min-cost-climbing-stairs/min-cost-climbing-stairs.cpp
@@ -3,21 +3,22 @@ using namespace std;
 
 class Solution {
 public:
-    vector<int> dp;
+   
 
-    int minCost(vector<int>& cost, int i) {
+    int minCost(vector<int>& cost, int i,vector<int>&dp) {
         if (i >= cost.size()) return 0;  // Base case: beyond the last step
         if (dp[i] != -1) return dp[i];   // Use memoized result
         
         // Choose 1-step or 2-steps
-        return dp[i] = cost[i] + min(minCost(cost, i + 1), minCost(cost, i + 2));
+        return dp[i] = cost[i] + min(minCost(cost, i + 1,dp), minCost(cost, i + 2,dp));
     }
 
     int minCostClimbingStairs(vector<int>& cost) {
         int n = cost.size();
-        dp.assign(n, -1);  // Initialize memoization array
+         vector<int> dp(10001,-1);
+          // Initialize memoization array
 
         // Start from either 0th or 1st step
-        return min(minCost(cost, 0), minCost(cost, 1));
+        return min(minCost(cost, 0, dp), minCost(cost, 1,dp));
     }
 };
