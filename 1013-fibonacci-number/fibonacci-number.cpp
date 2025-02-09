c2b71996a0eda73c8cb1a31a71bcdf4852876cce
vinay <vinayjjoshi.310@gmail.com>
2025-02-10 01:03:59 +0530

    Time: 0 ms (100.00%) | Memory: 8.2 MB (14.38%) - LeetSync

---
+++ 1013-fibonacci-number/fibonacci-number.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+ 
+ 
+    int fib(int n) {
+       
+       vector<int>dp(n+1,0);
+          if(n<=0) return 0;
+          
+          dp[0]=0;
+          dp[1]=1;
+ 
+       
+      for(int j=2;j<n+1;j++){ 
+        dp[j]=  dp[j-1]+dp[j-2] ;}
+       
+        return dp[n];
+    }
+};
