700cb72dc71070b8f12628cb2f6573cc988f7933
vinay <vinayjjoshi.310@gmail.com>
2025-02-10 01:04:17 +0530

    Time: 3 ms (67.96%) | Memory: 8.2 MB (14.38%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.cpp
+++ 1013-fibonacci-number/fibonacci-number.cpp
@@ -5,8 +5,8 @@ public:
     int fib(int n) {
        
        vector<int>dp(n+1,0);
-          if(n<=0) return 0;
-          
+          if(n<=1) return n;
+
           dp[0]=0;
           dp[1]=1;
  
