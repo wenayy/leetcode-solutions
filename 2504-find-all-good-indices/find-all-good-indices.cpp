5a21f8e600ed7e3ce03cb34809922e620f7b3e43
vinay <vinayjjoshi.310@gmail.com>
2025-03-17 23:04:36 +0530

    Time: 1287 ms (8.71%) | Memory: 93.5 MB (42.29%) - LeetSync

---
+++ 2504-find-all-good-indices/find-all-good-indices.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    vector<int> goodIndices(vector<int>& nums, int k) {
+        vector<int>ans;
+        int n=nums.size();
+        vector<int>increasing(n,1);
+        
+        vector<int>decreasing(n,1);
+        for(int i=1;i<nums.size();i++){
+            if(nums[i]<=nums[i-1]) decreasing[i]=decreasing[i-1]+1;
+            else continue;
+        }  
+        for(int i=n-2;i>=0;i--){
+            if(nums[i]<=nums[i+1]) increasing[i]=increasing[i+1]+1;
+            else continue;
+        } 
+        for(int i=0;i<n;i++){
+            cout<<decreasing[i] ;
+        }  
+         for(int i=0;i<n;i++){
+            cout<<increasing[i] ;
+        }  
+
+        for(int j=k;j<n-k;j++){
+            if(increasing[j+1]>=k && decreasing[j-1]>=k) {ans.push_back(j);}
+
+        } 
+        return ans;  
+
+        
+        
+    }
+};
