a243e125649b6e55fe5f752b14e6aeaed0218966
vinay <vinayjjoshi.310@gmail.com>
2025-03-18 01:09:56 +0530

    Time: 0 ms (100.00%) | Memory: 12.8 MB (57.14%) - LeetSync

---
+++ 2107-find-unique-binary-string/find-unique-binary-string.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    string findDifferentBinaryString(vector<string>& nums) {
+        int n=nums.size();
+        string ans;
+        for(int i=0;i<n;i++){
+            ans+=nums[i][i]^1;
+            
+        }
+        return ans;
+    }
+};
