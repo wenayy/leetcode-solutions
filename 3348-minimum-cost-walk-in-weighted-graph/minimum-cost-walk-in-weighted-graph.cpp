aea27bbb1b03f781c70a9eabae8ed7246547fa44
vinay <vinayjjoshi.310@gmail.com>
2025-03-21 01:41:58 +0530

    Time: 8 ms (98.08%) | Memory: 207.8 MB (66.56%) - LeetSync

--- 3348-minimum-cost-walk-in-weighted-graph/minimum-cost-walk-in-weighted-graph.cpp
+++ 3348-minimum-cost-walk-in-weighted-graph/minimum-cost-walk-in-weighted-graph.cpp
@@ -1,59 +1,55 @@
 class Solution {
 public:
-
     vector<int> parent;
+
     int find(int x) {
-        if(parent[x] == x)
-            return x;
-        
+        if (parent[x] == x) return x;
         return parent[x] = find(parent[x]);
     }
+
     void Union(int x, int y) {
         parent[y] = x;
     }
 
     vector<int> minimumCost(int n, vector<vector<int>>& edges, vector<vector<int>>& query) {
-        
         parent.resize(n);
-        vector<int> cost(n);  //Store "And operation" cost of each component
-	    
-	    for(int i = 0; i<n; i++) {
-	        parent[i] = i;
-            cost[i]   = -1;
-        }
-        
-        for (auto &edge : edges) {
-            int u = edge[0];
-            int v = edge[1];
-            int w = edge[2];
-            
+        vector<int> cost(n, -1); // Initialize cost to -1
+        iota(parent.begin(), parent.end(), 0); // Initialize parent array
+
+        for (int i = 0; i < edges.size(); i++) {
+            int u = edges[i][0];
+            int v = edges[i][1];
+            int w = edges[i][2];
+
             int parent_u = find(u);
             int parent_v = find(v);
-            
 
             if (parent_u != parent_v) {
-                cost[parent_u] &= cost[parent_v];
-                Union(parent_u, parent_v);
+                cost[parent_u] &= cost[parent_v]; // Update cost of parent_u with cost of parent_v
+                Union(parent_u, parent_v);        // Merge parent_v into parent_u
+                cost[parent_u] &= w;             // Update cost of parent_u with the current edge weight
+            } else {
+                cost[parent_u] &= w;             // If both nodes are in the same component, update the cost
             }
-
-            cost[parent_u] &= w;
         }
-        
-        vector<int> res;
-        for (auto &q : query) {
+
+        vector<int> result;
+        for (auto& q : query) {
             int s = q[0];
             int t = q[1];
-            
-            int p1 = find(s);
-            int p2 = find(t);
-            
-            if (s == t)
-                res.push_back(0);
-            else if (p1 != p2)
-                res.push_back(-1);
-            else
-                res.push_back(cost[p1]);
+
+            int parent_s = find(s);
+            int parent_t = find(t);
+
+            if (s == t) {
+                result.push_back(0);
+            } else if (parent_s != parent_t) {
+                result.push_back(-1);
+            } else {
+                result.push_back(cost[parent_s]);
+            }
         }
-        return res;
+
+        return result;
     }
 };
