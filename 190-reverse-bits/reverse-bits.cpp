ac626388e967ea9a522354f6d66e3fd64c7d95d6
vinay <vinayjjoshi.310@gmail.com>
2025-03-25 12:46:25 +0530

    Time: 0 ms (100.00%) | Memory: 7.9 MB (7.43%) - LeetSync

---
+++ 190-reverse-bits/reverse-bits.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    uint32_t reverseBits(uint32_t n) {
+        long long ans=0;
+      string k=bitset<32>(n).to_string();
+       int i=0;
+       int j=31;
+       while(i<=j){
+        swap(k[i],k[j]);
+        i++;
+        j--;
+       }
+   
+       ans=stoll(k, nullptr, 2);
+       return ans;
+
+        
+    }
+};
