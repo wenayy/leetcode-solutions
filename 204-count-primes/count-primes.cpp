3a8df1d22412a13f89d284b6d6794f5855c26631
vinay <vinayjjoshi.310@gmail.com>
2025-03-29 13:54:09 +0530

    Time: 171 ms (74.52%) | Memory: 12.6 MB (64.39%) - LeetSync

--- 204-count-primes/count-primes.cpp
+++ 204-count-primes/count-primes.cpp
@@ -5,7 +5,7 @@ public:
         primes[0]=primes[1]=false;
         for(int i=2;i*i<=n;i++){
             if(primes[i]){
-                for(int j=i*i;j<=n;j+=i){
+                for(int j=i+i;j<=n;j+=i){
                     primes[j]=false;
 
                 }
