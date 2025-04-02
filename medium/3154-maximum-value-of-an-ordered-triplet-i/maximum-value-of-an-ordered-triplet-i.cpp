6ba7ab32c1018068178301ea6e7978e106f6946e
vinay <vinayjjoshi.310@gmail.com>
2025-04-02 13:17:19 +0530

    Time: 3 ms (31.33%) | Memory: 21.6 MB (66.87%) - LeetSync

--- medium/3154-maximum-value-of-an-ordered-triplet-i/maximum-value-of-an-ordered-triplet-i.cpp
+++ medium/3154-maximum-value-of-an-ordered-triplet-i/maximum-value-of-an-ordered-triplet-i.cpp
@@ -6,8 +6,9 @@ public:
     for(int i=0;i<n;i++){
         for(int j=i+1;j<n;j++){
             for(int k=j+1;k<n;k++){
-               long long  temp=( long long)(nums[i]-nums[j])*nums[k];
-                
+             
+                long long r=nums[k];
+                  long long  temp= (nums[i]-nums[j])*r;
                 ans=max(temp,ans);
             }
         }
