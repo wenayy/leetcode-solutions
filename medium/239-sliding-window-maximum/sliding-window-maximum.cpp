67df0db8338b2a57e3ec4c0a32e90d99926c4600
vinay <vinayjjoshi.310@gmail.com>
2025-04-03 02:23:22 +0530

    Time: 388 ms (8.26%) | Memory: 211.7 MB (5.82%) - LeetSync

--- medium/239-sliding-window-maximum/sliding-window-maximum.cpp
+++ medium/239-sliding-window-maximum/sliding-window-maximum.cpp
@@ -1,26 +1,36 @@
 class Solution {
 public:
     vector<int> maxSlidingWindow(vector<int>& nums, int k) {
-        multiset<int> mp;
-        int n = nums.size();
-        vector<int> ans(n - k + 1);  // Correct size for result array
-        int j = k, r = 0;  // `j` starts at k, `r` is left pointer
+        multiset<int>mp;
+     
+        int n=nums.size();
+           vector<int>ans(n-k+1);
+        int j=k;
+        int r=0;
 
-        // Insert first k elements into multiset
-        for (int i = 0; i < k; i++) {
+        for(int i=0;i<k;i++){
             mp.insert(nums[i]);
-        }
-        ans[0] = *mp.rbegin();  // Max of the first window
 
-        // Start sliding the window
-        while (j < n) {
-            mp.erase(mp.find(nums[r]));  // Remove element going out of window
-            r++;  // Move left pointer
-            mp.insert(nums[j]);  // Insert new element coming into window
-            ans[r] = *mp.rbegin();  // Store max value
-            j++;  // Move right pointer
         }
+        ans[0]=*mp.rbegin();
 
+        while(j<n){
+            mp.erase(mp.find(nums[r])); 
+            r+=1;
+             
+            
+            mp.insert(nums[j]);
+             j+=1;
+           
+
+            ans[r]= *mp.rbegin();
+
+
+
+
+
+        }
         return ans;
+        
     }
 };
