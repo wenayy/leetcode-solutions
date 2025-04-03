640460fbe17beb739e0605daa46a037b5aeb9ed0
vinay <vinayjjoshi.310@gmail.com>
2025-04-03 18:15:44 +0530

    Time: 29 ms (5.12%) | Memory: 93.2 MB (10.66%) - LeetSync

---
+++ medium/2138-sum-of-beauty-in-the-array/sum-of-beauty-in-the-array.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int sumOfBeauties(vector<int>& nums) {
+        int n=nums.size();
+        vector<int>left (n, nums[0]);
+             vector<int>right (n,nums[n - 1]);
+        for(int i=1;i<n;i++){
+            
+           left[i]=max(left[i-1],nums[i-1]);
+         right[n-i-1]=min(right[n-i],nums[n-i]);
+
+
+        }
+        int ans=0;
+        for(int i=1;i<n-1;i++){
+            if(left[i]<nums[i] && right[i]>nums[i])  ans+=2;
+            else  if(nums[i-1]<nums[i] && nums[i]<nums[i+1]) ans+=1;
+           
+        }
+
+        
+        return ans; 
+        
+    }
+};
