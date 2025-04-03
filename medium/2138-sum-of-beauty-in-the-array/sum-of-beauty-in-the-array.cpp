c6b2c2c1115af4cb2050e27c8fccfa69fb7d58cd
vinay <vinayjjoshi.310@gmail.com>
2025-04-03 18:37:12 +0530

    Time: 8 ms (43.07%) | Memory: 93 MB (19.83%) - LeetSync

--- medium/2138-sum-of-beauty-in-the-array/sum-of-beauty-in-the-array.cpp
+++ medium/2138-sum-of-beauty-in-the-array/sum-of-beauty-in-the-array.cpp
@@ -3,11 +3,11 @@ public:
     int sumOfBeauties(vector<int>& nums) {
         int n=nums.size();
         vector<int>left (n, 0);
-             vector<int>right (n,nums[n-1]);
+             vector<int>right (n,nums[n-1]); // because min compare karna hai so min(0, 3) = 0; y ho jaayega which iwll give wrong answer 
         for(int i=1;i<n;i++){
             
            left[i]=max(left[i-1],nums[i-1]);
-         right[n-i-1]=min(right[n-i],nums[n-i]);
+         right[n-i-1]=min(right[n-i],nums[n-i]); // min because if min se hi bada hoga toh baankiyon se toh bada hoga hi,
 
 
         }
