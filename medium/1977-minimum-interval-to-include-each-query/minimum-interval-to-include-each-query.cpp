8c433fb022842a536efd2aa9e36b2dcb6db96c18
vinay <vinayjjoshi.310@gmail.com>
2025-04-05 04:09:36 +0530

    Time: 85 ms (79.97%) | Memory: 117.3 MB (77.61%) - LeetSync

--- medium/1977-minimum-interval-to-include-each-query/minimum-interval-to-include-each-query.cpp
+++ medium/1977-minimum-interval-to-include-each-query/minimum-interval-to-include-each-query.cpp
@@ -1,45 +1,43 @@
 class Solution {
 public:
     vector<int> minInterval(vector<vector<int>>& intervals, vector<int>& queries) {
-        int n = queries.size();
-        vector<int> answer(n, -1);
-
-        // Store original indices of queries so we can restore order later
-        vector<pair<int, int>> q_with_idx;
-        for (int i = 0; i < n; i++) {
-            q_with_idx.push_back({queries[i], i});
-        }
-
-        // Sort queries by value
-        sort(q_with_idx.begin(), q_with_idx.end());
-
-        // Sort intervals by their left endpoint
+        // Sort intervals by start
         sort(intervals.begin(), intervals.end());
 
-        // Min-heap: {interval size, right}
-        priority_queue<pair<int, int>, vector<pair<int, int>>, greater<>> minHeap;
-        int i = 0;
+        // Pair each query with its index to restore order later
+        vector<pair<int, int>> queryWithIndex;
+        for (int i = 0; i < queries.size(); ++i) {
+            queryWithIndex.push_back({queries[i], i});
+        }
 
-        for (auto& [query, idx] : q_with_idx) {
-            // Add intervals whose left <= query
+        // Sort queries
+        sort(queryWithIndex.begin(), queryWithIndex.end());
+
+        // Min heap to store {size, end}
+        priority_queue<pair<int, int>, vector<pair<int, int>>, greater<>> minHeap;
+        vector<int> result(queries.size(), -1);
+
+        int i = 0; // Pointer for intervals
+        for (auto& [query, idx] : queryWithIndex) {
+            // Push intervals that start <= query
             while (i < intervals.size() && intervals[i][0] <= query) {
                 int l = intervals[i][0], r = intervals[i][1];
-                int size = r - l + 1;
-                minHeap.push({size, r});
-                i++;
+                if ( query <= r) {
+                    minHeap.push({r - l + 1, r});
+                }
+                ++i;
             }
 
-            // Remove intervals that don't contain the query
+            // Pop intervals from heap that end < query
             while (!minHeap.empty() && minHeap.top().second < query) {
                 minHeap.pop();
             }
 
-            // Top of the heap is the smallest interval that contains query
             if (!minHeap.empty()) {
-                answer[idx] = minHeap.top().first;
+                result[idx] = minHeap.top().first; // size of smallest interval covering query
             }
         }
 
-        return answer;
+        return result;
     }
 };
