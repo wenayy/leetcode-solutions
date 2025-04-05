bde00e5844d4aca5cceeb2fd936e3ecd5a0672c8
vinay <vinayjjoshi.310@gmail.com>
2025-04-05 15:29:29 +0530

    Time: 0 ms (100.00%) | Memory: 14.1 MB (7.90%) - LeetSync

---
+++ medium/74-search-a-2d-matrix/search-a-2d-matrix.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        int m=matrix.size();
+        int n=matrix[0].size();
+        vector<int> merged;
+        merged.reserve(m * n); 
+       
+    
+for (int i = 0; i < m; i++) {
+    for (int j = 0; j < n; j++) {
+        merged.push_back(matrix[i][j]);
+    }
+}
+        int i=0;
+        cout<<merged.size();
+        int j= merged.size()-1;
+        while(i<=j){
+           
+            int mid=i+((j-i)/2);
+            if(merged[mid]==target) return true;
+            else if(merged[mid]<target) i=mid+1;
+            else j=mid-1;
+        }
+
+        return false;
+        
+    }
+};
