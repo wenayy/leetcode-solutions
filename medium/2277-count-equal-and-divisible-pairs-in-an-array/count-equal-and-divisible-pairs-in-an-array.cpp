81f92d8f3c4c17762197ddf3d647a2e3bdd659af
vinay <vinayjjoshi.310@gmail.com>
2025-04-17 18:38:03 +0530

    Time: 3 ms (28.60%) | Memory: 16.9 MB (6.09%) - LeetSync

--- medium/2277-count-equal-and-divisible-pairs-in-an-array/count-equal-and-divisible-pairs-in-an-array.cpp
+++ medium/2277-count-equal-and-divisible-pairs-in-an-array/count-equal-and-divisible-pairs-in-an-array.cpp
@@ -1,15 +1,34 @@
 class Solution {
 public:
     int countPairs(vector<int>& nums, int k) {
-        int n=nums.size();
-      
-      int ans=0;
-        for(int i=0;i<n;i++){
-   
-            for(int j=i+1;j<n;j++){
-                if((( i*j)%k==0) && (nums[i]==nums[j]))  ans++;
+        unordered_map<int, vector<int>> mp;
+        for (int i = 0; i < nums.size(); ++i) {
+            mp[nums[i]].push_back(i);
+        }
+
+        int ans = 0;
+        for (auto& [val, idxList] : mp) {
+            unordered_map<int, int> remFreq;
+
+            // Step 1: Count remainders mod k
+            for (int i : idxList) {
+                remFreq[i % k]++;
+            }
+
+            // Step 2: For each pair of remainders
+            for (auto& [r1, c1] : remFreq) {
+                for (auto& [r2, c2] : remFreq) {
+                    if ((r1 * r2) % k == 0) {
+                        if (r1 == r2) {
+                            ans += (c1 * (c1 - 1)) / 2; // Choose 2 from c1
+                        } else if (r1 < r2) {
+                            ans += c1 * c2; // Unique pairs
+                        }
+                    }
+                }
             }
         }
+
         return ans;
     }
 };
