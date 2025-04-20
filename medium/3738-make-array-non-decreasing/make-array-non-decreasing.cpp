0e764f74c2e3a1e7e70423b0f8f1e52bd8fa6b1e
vinay <vinayjjoshi.310@gmail.com>
2025-04-21 03:01:03 +0530

    Time: 117 ms (5.02%) | Memory: 224.6 MB (5.28%) - LeetSync

--- medium/3738-make-array-non-decreasing/make-array-non-decreasing.cpp
+++ medium/3738-make-array-non-decreasing/make-array-non-decreasing.cpp
@@ -1,16 +1,29 @@
 class Solution {
 public:
     int maximumPossibleSize(vector<int>& nums) {
+        vector<int>temp(nums.begin(),nums.end());
+        sort(temp.begin(),temp.end()) ;
+        if(temp==nums) return nums.size();
+        int max=nums[0]
+;
+ 
+int count=0;
+int n=nums.size();
+vector<int>modified(n,nums[0]);
+for(int i=1;i<nums.size();i++){
+    if(nums[i]>max) {max=nums[i]; modified[i]=nums[i];} 
+    else modified[i]=max;
 
-        int size = 0, prev = -1;
+     
+}
+for(int x=0;x<n;x++){
+   if (nums[x] ==modified[x]) count++;
+}
+      
 
-        for(auto num : nums) {
-            if(num >= prev) {
-                prev = num;
-                size++;
-            }
-        }
 
-        return size;
+return count;
+
+        
     }
 };
