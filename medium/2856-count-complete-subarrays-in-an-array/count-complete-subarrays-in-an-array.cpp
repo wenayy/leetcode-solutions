723bc7a48a4ae4cf4ffa480b17a2d9e33f0bfebf
vinay <vinayjjoshi.310@gmail.com>
2025-04-24 10:08:11 +0530

    Time: 47 ms (25.57%) | Memory: 46.8 MB (27.31%) - LeetSync

---
+++ medium/2856-count-complete-subarrays-in-an-array/count-complete-subarrays-in-an-array.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    int countCompleteSubarrays(vector<int>& nums) {
+        set<int> mp(nums.begin(), nums.end()); // total distinct
+        multiset<int> ms;
+        set<int> ms2;
+
+        int i = 0, j = 0, n = nums.size(), ans = 0;
+
+        while (i < n) {
+            ms.insert(nums[i]);
+            ms2.insert(nums[i]);
+
+            if (ms2.size() == mp.size()) {
+                ans += n - i;
+
+                while (j <= i && ms2.size() == mp.size()) {
+                    // Erase one instance from multiset
+                    ms.erase(ms.find(nums[j]));
+
+                    // Check if that value still exists in multiset
+                    if (ms.find(nums[j]) == ms.end()) {
+                        ms2.erase(nums[j]);
+                    }
+
+                    j++;
+                    if (ms2.size() == mp.size()) {
+                        ans += n - i;
+                    }
+                }
+            }
+
+            i++;
+        }
+
+        return ans;
+    }
+};
