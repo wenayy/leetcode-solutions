56152de7a213d06acb987dce15e0080e06f103a1
vinay <vinayjjoshi.310@gmail.com>
2025-04-27 14:27:08 +0530

    Time: 222 ms (5.04%) | Memory: 48.5 MB (45.56%) - LeetSync

--- medium/3685-count-subarrays-of-length-three-with-a-condition/count-subarrays-of-length-three-with-a-condition.cpp
+++ medium/3685-count-subarrays-of-length-three-with-a-condition/count-subarrays-of-length-three-with-a-condition.cpp
@@ -4,7 +4,8 @@ public:
         int ans=0;
         for(int i=0;i<=nums.size()-3;i++){
             int temp=nums[i]+nums[i+2];
-            if(temp==(nums[i+1]/2.0)) ans++;
+            cout<<temp<<nums[i+1]<<endl;
+            if(temp==(double(nums[i+1])/2)) ans++;
         }
         return ans;
     }
