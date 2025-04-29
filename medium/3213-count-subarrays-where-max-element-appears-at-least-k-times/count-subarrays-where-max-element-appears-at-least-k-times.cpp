2040e53d8391cdff734039b12664c6842cf46a83
vinay <vinayjjoshi.310@gmail.com>
2025-04-29 10:12:01 +0530

    Time: 7 ms (44.58%) | Memory: 121.8 MB (48.26%) - LeetSync

---
+++ medium/3213-count-subarrays-where-max-element-appears-at-least-k-times/count-subarrays-where-max-element-appears-at-least-k-times.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    long long countSubarrays(vector<int>& nums, int k) {
+        
+        int mx=*max_element(nums.begin(),nums.end());
+        int i=0;
+        int j=0;
+        int n=nums.size();
+        long long count=0;
+        long long ans=0;
+        while(i<n){
+            if(nums[i]==mx) count++;
+            while(count>=k){
+                ans+=(n-i);
+                if(nums[j]==mx) count--;
+                j++;
+
+            }
+            i++;
+
+        }
+        return ans;
+    }
+};
